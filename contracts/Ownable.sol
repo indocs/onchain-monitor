eadcd757cd2430e6c21a6dfcb2f0eb4495cb9501
Oksana Kryschuk <kovart.public@gmail.com>
2025-04-27 09:21:00 +0000

    refactor: update Ownable.sol

---
+++ contracts/Ownable.sol
@@ -0,0 +1,44 @@
+// Minimal Ownable contract to provide access control for future enhancements
+// This file introduces an owner and an onlyOwner modifier that can be used by
+// existing and future contracts in this repo for security tightening.
+pragma solidity ^0.8.0;
+
+/**
+ * @title Ownable
+ * @dev Simple contract with an owner address, and basic transferOwnership functionality.
+ */
+contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
